67712fe3d25516b3fd78b24e36d011eba30c971f
yottaflops <yottaflops89@gmail.com>
2014-12-23 22:31:49 -0500

    view items by batch

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    if params[:by_batch]
+      @batches = ClearanceBatch.all
+      @batchless_items = Item.where(clearance_batch_id: nil)
+      return render :batch_index
+    else
+      @items = Item.all.order(:status)
+    end
   end
 end
