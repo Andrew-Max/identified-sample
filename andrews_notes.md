bb198ea6fa49207d01472e310bf3251ea9af311d
yottaflops <yottaflops89@gmail.com>
2014-12-26 19:28:36 -0500

    items feature spec

---
+++ andrews_notes.md
@@ -0,0 +1,5 @@
+# Notes
+
+## ClearanceBatches Controller
+
+- One of my bigg
--- spec/factories.rb
+++ spec/factories.rb
@@ -6,9 +6,11 @@ FactoryGirl.define do
 
   factory :item do
     style
+    clearance_batch
     color "Blue"
     size "M"
     status "sellable"
+    price_sold 23
   end
 
   factory :style do
